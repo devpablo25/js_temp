bee59fe8d8c99414c2d8a33334b5e0d20be7ee80
devpablo25 <rcdmine@gmail.com>
2025-03-06 17:08:54 -0500

    finally got the converson working

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 document.getElementById("convert").addEventListener("click", convert)
+
 function convert() {
+
+    var input = document.getElementById("textbox").value
     if (document.getElementById("toF").checked === true) {
+            console.log("to f");
+            document.getElementById("Output").textContent = (input * 9/5 ) + 32;
 
-        console.log("help");
-
+    } else if (document.getElementById("toC").checked === true){ 
+        
+        
+        document.getElementById("Output").textContent = (input - 32) * 5/9;
+       
     }
-}
+} 
+
